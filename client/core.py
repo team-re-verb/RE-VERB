db573ed13cbcd7b56e892f3c2fbfdb0365404b82
Matan Yesharim <matan.yesharim@gmail.com>
2018-11-29 17:46:32 +0200

    Initial Commit

---
+++ client/core.py
@@ -0,0 +1,8 @@
+import json
+import request
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ server/server.py
@@ -0,0 +1,8 @@
+import json
+import socket
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
