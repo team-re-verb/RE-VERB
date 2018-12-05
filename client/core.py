c5d68911475570e05bea4d5d77970327add8efd9
ofirnaca <ofirnaca@gmail.com>
2018-12-05 17:45:16 +0200

    fixed more stuff

--- client/core.py
+++ client/core.py
@@ -2,15 +2,11 @@
 
 import json
 import requests
-<<<<<<< HEAD
 import base64
 
 from recorder import read_audio,record_audio
-
 RECORD_FILENAME = "audio/record.wav"
 
-=======
->>>>>>> 02544c90e2a4a916cd0ed171c2ae3e248a3ea1d3
 
 def main():
 
@@ -23,9 +19,5 @@ def main():
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     #record_audio(5, RECORD_FILENAME)
     main()
-=======
-    main()
->>>>>>> 02544c90e2a4a916cd0ed171c2ae3e248a3ea1d3
