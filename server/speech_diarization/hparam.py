7b7bc9ba172ebf5514bc614cc0ff11442320fe22
Tralfazz <matan.yesharim@gmail.com>
2019-04-27 20:52:52 +0300

    yaml load_all() deprecated

--- server/speech_diarization/hparam.py
+++ server/speech_diarization/hparam.py
@@ -3,7 +3,7 @@ import yaml
 
 def load_hparam(filename):
     stream = open(filename, 'r')
-    docs = yaml.load_all(stream)
+    docs = yaml.load_all(stream, Loader=yaml.FullLoader) # load all without Loader is deprecated
     hparam_dict = dict()
     for doc in docs:
         for k, v in doc.items():
